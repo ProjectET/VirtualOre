ca615d2ee66fd6d4dab69abbff68ac19cb7f27fe
ProjectET <unconfigured@null.spigotmc.org>
2021-04-04 12:44:44 +0930

    I dont know what i am doing... Part 2: Electric Boogaloo

--- src/main/java/io/github/projectet/virtualore/blocks/entity/oreBlockEntity.java
+++ src/main/java/io/github/projectet/virtualore/blocks/entity/oreBlockEntity.java
@@ -1,4 +1,44 @@
 package io.github.projectet.virtualore.blocks.entity;
 
-public class oreBlockEntity {
+import net.fabricmc.fabric.api.block.entity.BlockEntityClientSerializable;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.nbt.CompoundTag;
+/*
+TODO
+Remember to use the PlayerBlockBreakEvents from net.fabricmc.fabric.api.event.player;
+Consider using ActionResult in conjunction with the event.
+Consider using BlockStates for storing the int of the yield or consider using Cardinal components.
+*/
+
+public class oreBlockEntity extends BlockEntity implements BlockEntityClientSerializable {
+
+    public oreBlockEntity(BlockEntityType<?> type) {
+        super(type);
+    }
+
+
+    @Override
+    public void fromClientTag(CompoundTag tag) {
+
+    }
+
+    @Override
+    public CompoundTag toClientTag(CompoundTag tag) {
+        return null;
+    }
+
+
+    @Override
+    public void fromTag(BlockState state, CompoundTag tag) {
+
+    }
+
+    @Override
+    public CompoundTag toTag(CompoundTag tag) {
+    return null;
+    }
+
 }
+
--- src/main/java/io/github/projectet/virtualore/blocks/oreBlock.java
+++ src/main/java/io/github/projectet/virtualore/blocks/oreBlock.java
@@ -1,10 +1,10 @@
 package io.github.projectet.virtualore.blocks;
 
-import io.github.projectet.virtualore.blocks.blockEntities.oreBlockEntity;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockEntityProvider;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.world.BlockView;
+import org.jetbrains.annotations.Nullable;
 
 public class oreBlock extends Block implements BlockEntityProvider {
 
@@ -12,9 +12,9 @@ public class oreBlock extends Block implements BlockEntityProvider {
         super(settings);
     }
 
+    @Nullable
     @Override
-    public BlockEntity createBlockEntity(BlockView blockView) {
-        return new oreBlockEntity(oreBlockEntity.ORE_BLOCK_TYPE);
+    public BlockEntity createBlockEntity(BlockView world) {
+        return null;
     }
-
 }
